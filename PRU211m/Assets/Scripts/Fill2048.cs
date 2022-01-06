b96e71c5e3ea1b8e823d9e2176bd51738a5ea1a2
khoatvbattu <58596136+khoatvbattu@users.noreply.github.com>
2022-01-06 21:10:00 +0700

    Move fill object
    Di chuyển hình ảnh khi press WASD

--- PRU211m/Assets/Scripts/Fill2048.cs
+++ PRU211m/Assets/Scripts/Fill2048.cs
@@ -7,12 +7,22 @@ public class Fill2048 : MonoBehaviour
 {
     public int value;
     [SerializeField] Text valueDisplay;
-
+    [SerializeField] float speed;
     public void FillValueUpdate(int valueIn)
     {
         value = valueIn;
 
         // Update value display
         valueDisplay.text = value.ToString();
+    }
+
+    // Moving the fill object
+    private void Update()
+    {
+        if(transform.localPosition != Vector3.zero)
+        {
+            // Update the local position of this fill object
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, Vector3.zero, speed * Time.deltaTime);
+        }
     }
 }
