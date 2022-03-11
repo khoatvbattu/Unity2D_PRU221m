45b6815dcd4b873b006be558a4b6173ebfc54b30
khoatvbattu <58596136+khoatvbattu@users.noreply.github.com>
2022-03-11 15:22:23 +0700

    2048 2nd
    Another version of 2048

---
+++ PRU211m/Assets/Scripts/2048_2/MainScreen_2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScreen_2 : MonoBehaviour
+{
+    [SerializeField] GameObject mainScreen_2;
+    [SerializeField] GameObject game2048_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void restart2048()
+    {
+        mainScreen_2.SetActive(true);
+        SceneManager.LoadScene(0);
+        mainScreen_2.SetActive(false);
+        game2048_2.SetActive(true);
+    }
+
+    public void display2048()
+    {
+        mainScreen_2.SetActive(false);
+        game2048_2.SetActive(true);
+    }
+
+    public void backMain()
+    {
+        SceneManager.LoadScene(0);
+        game2048_2 .SetActive(false);
+        mainScreen_2.SetActive(true);
+    }
+}
